404628eb1cc0d74e69645c3826c85a815c9e0656
BenD2525 <bendawson2525@gmail.com>
2022-11-29 11:23:38 +0000

    Restructure home page and add link to reviews page, add basic structure of reviews page

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     ''' Returns the home page.'''
     return render(request, 'home/index.html')
+
+
+def reviews(request):
+    ''' Returns the home page.'''
+    return render(request, 'home/reviews.html')

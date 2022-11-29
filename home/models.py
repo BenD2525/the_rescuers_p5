7944fbc81673266dbefc8066dedbc76eff3ae4cb
BenD2525 <bendawson2525@gmail.com>
2022-11-29 13:26:57 +0000

    Add for loop to reviews page, add view to display review model, add edit/delete buttons

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Reviews(models.Model):
+    '''Model which stores the website reviews.'''
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Reviews
 
 
 def home(request):
@@ -7,5 +8,23 @@ def home(request):
 
 
 def reviews(request):
-    ''' Returns the home page.'''
-    return render(request, 'home/reviews.html')
+    ''' Returns the reviews page.'''
+
+    serialized_reviews = []
+
+    reviews = Reviews.objects.all()
+
+    for review in reviews:
+        serialized_reviews.append({
+            "title": review.title,
+            "content": review.content,
+            "user": review.user,
+            "created": review.created,
+            "updated": review.updated,
+        })
+
+    context = {
+        "reviews": serialized_reviews
+        }
+    print(serialized_reviews)
+    return render(request, 'home/reviews.html', context)

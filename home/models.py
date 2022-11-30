3935cdb141b82d32c0c6ea2922765ad554b247df
BenD2525 <bendawson2525@gmail.com>
2022-11-30 12:07:02 +0000

    Add crispy forms, styld edit/add/delete review template, fix urls for add/edit/delete review views

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Reviews(models.Model):
@@ -12,5 +13,9 @@ class Reviews(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        '''Defines a redirect URL after editing/deleting.'''
+        return reverse('home:reviews')
 
 

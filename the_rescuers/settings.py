57196f13b42de1df20375f9e2bbec9d332987569
BenD2525 <bendawson2525@gmail.com>
2022-12-04 15:39:56 +0000

    Remove star from allowed hosts in settings.py

--- the_rescuers/settings.py
+++ the_rescuers/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['the-rescuers-p5.herokuapp.com', 'localhost', *]
+ALLOWED_HOSTS = ['the-rescuers-p5.herokuapp.com', 'localhost']
 
 
 # Application definition

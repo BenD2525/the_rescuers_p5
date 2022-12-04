bee724268f17f13118abd30e6c571fa06dc41331
BenD2525 <bendawson2525@gmail.com>
2022-12-04 17:15:03 +0000

    Remove url for favicons from urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
 from . import views
 
 
 app_name = 'home'
 
 urlpatterns = [
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('', views.home, name='home'),
     path('reviews', views.reviews, name='reviews'),
     path('reviews/add', views.AddReview.as_view(), name='add_review'),
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
 from . import views
 
 
@@ -7,5 +6,4 @@ app_name = 'products'
 
 urlpatterns = [
     path('products', views.products, name='products'),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
 ]

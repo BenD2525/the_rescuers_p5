ab247d68e41a66b8f5d225efc486c20e63a05240
BenD2525 <bendawson2525@gmail.com>
2022-12-04 18:29:30 +0000

    Remove staticroot from settings.py

--- the_rescuers/settings.py
+++ the_rescuers/settings.py
@@ -95,7 +95,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -159,7 +158,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

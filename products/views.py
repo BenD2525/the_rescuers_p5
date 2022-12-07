03ce9dbf52fff53a16cc84635acf0053a3de424c
BenD2525 <bendawson2525@gmail.com>
2022-12-07 16:24:00 +0000

    Add search/filter functionality to product list, add message for searches which find 0 products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse
+from django.contrib import messages
 from .models import Product
+from django.db.models import Q
 
 
 def products_list(request):
     ''' Returns the products page.'''
     serialized_products = []
     products = Product.objects.all()
+    query = None
 
     for product in products:
         serialized_products.append({
@@ -14,11 +17,21 @@ def products_list(request):
             "price": product.price,
             "image_url": product.image_url,
             "id": product.id,
-        })     
-    context = {
-        "products": serialized_products
-        }
+        })  
 
+    if 'search' in request.GET:
+        query = request.GET['search']
+        if not query:
+            messages.error(request, "Please enter search criteria")
+            return redirect(reverse('products:products_list'))
+        
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        serialized_products = products.filter(queries)
+
+    context = {
+        "products": serialized_products,
+        "search": query, 
+        }
     return render(request, 'products/products_list.html', context)
 
 

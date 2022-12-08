2a4572734e39d2d5323149c26de399f40057d9cf
BenD2525 <bendawson2525@gmail.com>
2022-12-08 11:06:21 +0000

    Add Resident model to home app

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Reviews
+from .models import Reviews, Resident
 
 admin.site.register(Reviews)
+admin.site.register(Resident)
--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from products.models import Product
 
 
 class Reviews(models.Model):
@@ -18,4 +19,17 @@ class Reviews(models.Model):
         '''Defines a redirect URL after editing/deleting.'''
         return reverse('home:reviews')
 
+
+class Resident(models.Model):
+    '''Model which stores the residents of the shelter.'''
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    age = models.IntegerField()
+    favourite_toy = models.ForeignKey(Product, on_delete=models.CASCADE)
+    updated = models.DateField(auto_now=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 

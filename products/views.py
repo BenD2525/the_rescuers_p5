98f28497ce7c50fc5a7cdef055fe12168a9a2c83
BenD2525 <bendawson2525@gmail.com>
2022-12-08 14:05:54 +0000

    Add product sorting functionality to products_list view and products_list template

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render, reverse
 from django.contrib import messages
 from .models import Product
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def products_list(request):
@@ -9,6 +10,8 @@ def products_list(request):
     serialized_products = []
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
 
     for product in products:
         serialized_products.append({
@@ -19,18 +22,34 @@ def products_list(request):
             "id": product.id,
         })  
 
-    if 'search' in request.GET:
-        query = request.GET['search']
-        if not query:
-            messages.error(request, "Please enter search criteria")
-            return redirect(reverse('products:products_list'))
-        
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        serialized_products = products.filter(queries)
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            serialized_products = products.order_by(sortkey)
+
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "Please enter search criteria")
+                return redirect(reverse('products:products_list'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            serialized_products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         "products": serialized_products,
-        "search": query, 
+        "search": query,
+        'current_sorting': current_sorting,
         }
     return render(request, 'products/products_list.html', context)
 

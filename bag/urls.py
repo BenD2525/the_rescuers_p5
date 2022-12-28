98682191b0ce209ee5803d264959cfd15a62d4d9
BenD2525 <bendawson2525@gmail.com>
2022-12-28 19:20:11 +0000

    Add update and remove script to bag, allowing for bag to be amended

--- bag/urls.py
+++ bag/urls.py
@@ -7,4 +7,6 @@ app_name = 'bag'
 urlpatterns = [
     path('', views.bag, name='bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('amend/<item_id>/', views.amend_bag, name='amend_bag'),
+    path('remove/<item_id>/', views.remove_bag, name='remove_bag'),
 ]

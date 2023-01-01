9eae7c91e3642e48695be586feee304e814c4fa9
BenD2525 <bendawson2525@gmail.com>
2023-01-01 12:51:09 +0000

    Create Order and OrderDetail models in checkout models.py

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
+
+
+class Order(models.Model):
+    """
+       Model for Orders, created when checking out the bag.
+    """
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField(max_length=250, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    street_address_1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address_2 = models.CharField(max_length=80, null=True, blank=True)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=10, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = models.CharField(max_length=40, null=False, blank=False)  
+
+
+class OrderDetail(models.Model):
+    """
+       Model for Order Detail, refers to the product model and details being checked out.
+    """
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='details')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)

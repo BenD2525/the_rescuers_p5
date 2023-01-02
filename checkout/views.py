282a4579a1ee0b3e999a4930cce6588cf82ace55
BenD2525 <bendawson2525@gmail.com>
2023-01-02 11:43:53 +0000

    Add Order form to checkout page, update checkout view to display order form and redirect if bag is empty

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from products.models import Product
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 
 def checkout(request):
-    ''' Returns the checkout page.'''
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products:products_list'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)

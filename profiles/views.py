96fb15b00a2d9d4fd329e904bb1e6f2319c3c058
BenD2525 <bendawson2525@gmail.com>
2023-01-10 08:36:46 +0000

    Create basic view and template for userprofile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """ Displays the user's profile. """
+    template = 'profiles/user_profile.html'
+    context = {}
+
+    return render(request, template, context)

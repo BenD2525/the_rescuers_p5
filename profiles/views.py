acb548fd7f897a54ba437928155094aad4782670
BenD2525 <bendawson2525@gmail.com>
2023-01-10 14:13:40 +0000

    Register UserProfile with admin, add context to user_profile view, set up list to display current userprofile in template

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from .models import UserProfile
 
 
 def user_profile(request):
     """ Displays the user's profile. """
+    profile = UserProfile.objects.get(user=request.user)
+    print(profile)
     template = 'profiles/user_profile.html'
-    context = {}
+    context = {
+        'city': profile.default_city,
+        'email': profile.default_email,
+        'phone_number': profile.default_phone_number,
+        'address_1': profile.default_street_address_1,
+        'address_2': profile.default_street_address_2,
+        'postcode': profile.default_postcode,
+        'county': profile.default_county,
+        'country': profile.default_country,
+
+    }
 
     return render(request, template, context)

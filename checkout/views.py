7ad7e161207ddf64056c2fa58e59d86e30890261
BenD2525 <bendawson2525@gmail.com>
2023-01-21 20:57:57 +0000

    Add media query to resize product detail images on smaller screens

--- checkout/views.py
+++ checkout/views.py
@@ -103,8 +103,9 @@ def thank_you(request):
     # making an order
     if 'redirected_from_order_success' not in request.session:
         return render(request, "404.html")
-    # Clear the bag now that the order has been created
+    # Clear the bag and redirection token now that the order has been created
     request.session.pop('bag', None)
+    request.session.pop('redirected_from_order_success', None)
     return render(request, 'checkout/thank_you.html')
 
 
